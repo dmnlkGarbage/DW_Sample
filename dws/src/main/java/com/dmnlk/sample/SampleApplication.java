e4ebcf0e05d2b9bc6c218715e997f08fca3fdb27
dmnlk <seikima2demon@gmail.com>
2015-07-09 00:29:18 +0900

    add Application Class

--- dws/src/main/java/com/dmnlk/sample/SampleApplication.java
+++ dws/src/main/java/com/dmnlk/sample/SampleApplication.java
@@ -1,7 +1,30 @@
 package com.dmnlk.sample;
 
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
 /**
- * Created by masaki on 2015/07/08.
+ * @author dmnlk
  */
-public class SampleApplication {
+public class SampleApplication extends Application<SampleConfiguration> {
+    public static void main(String[] args) throws  Exception {
+        new SampleApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello";
+    }
+
+    @Override
+    public void initialize(Bootstrap<SampleConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(SampleConfiguration configuration,
+                    Environment environment) {
+        // nothing to do yet
+    }
 }
