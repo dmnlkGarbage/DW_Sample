3360f23a112f90af6d50bb9cfb780e49a9951d5f
dmnlk <seikima2demon@gmail.com>
2015-07-09 00:29:09 +0900

    add Configuration Class

--- dws/src/main/java/com/dmnlk/sample/SampleConfiguration.java
+++ dws/src/main/java/com/dmnlk/sample/SampleConfiguration.java
@@ -1,7 +1,33 @@
 package com.dmnlk.sample;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
 /**
- * Created by masaki on 2015/07/08.
+ * @author dmnlk
  */
-public class SampleConfiguration {
+public class SampleConfiguration extends Configuration {
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+    @JsonProperty
+    public void setDefaultName(String defaultName) {
+        this.defaultName = defaultName;
+    }
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
 }
