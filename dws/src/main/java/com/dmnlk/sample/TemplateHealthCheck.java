a41a76d6b57d5cceaa7d20e5082d577f294f94bf
dmnlk <seikima2demon@gmail.com>
2015-07-12 00:06:25 +0900

    modified Sample HealthCheck

--- dws/src/main/java/com/dmnlk/sample/TemplateHealthCheck.java
+++ dws/src/main/java/com/dmnlk/sample/TemplateHealthCheck.java
@@ -8,8 +8,16 @@ import com.codahale.metrics.health.HealthCheck;
 public class TemplateHealthCheck extends HealthCheck {
     private final String template;
 
+    public TemplateHealthCheck(String template) {
+        this.template = template;
+    }
+
     @Override
     protected Result check() throws Exception {
-        return null;
+        final String saying = String.format(template, "TEST");
+        if (saying.contains("TEST") == false) {
+            return  Result.unhealthy("template doesn't include a name");
+        }
+        return Result.healthy();
     }
 }
