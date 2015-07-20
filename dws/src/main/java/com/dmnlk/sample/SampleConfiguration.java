ffbf34f60aef3975c0e0c73c3450dd3c663bad49
dmnlk <seikima2demon@gmail.com>
2015-07-21 01:06:39 +0900

    add DataSouceFactory

--- dws/src/main/java/com/dmnlk/sample/SampleConfiguration.java
+++ dws/src/main/java/com/dmnlk/sample/SampleConfiguration.java
@@ -2,7 +2,12 @@ package com.dmnlk.sample;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+
+import io.dropwizard.db.DataSourceFactory;
 import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author dmnlk
@@ -13,6 +18,20 @@ public class SampleConfiguration extends Configuration {
 
     @NotEmpty
     private String defaultName = "Stranger";
+
+
+    @Valid
+    @NotNull
+    @JsonProperty
+    private DataSourceFactory dataSourceFactory;
+
+    public DataSourceFactory getDataSourceFactory() {
+        return dataSourceFactory;
+    }
+    
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+        this.dataSourceFactory = dataSourceFactory;
+    }
 
     @JsonProperty
     public String getDefaultName() {
