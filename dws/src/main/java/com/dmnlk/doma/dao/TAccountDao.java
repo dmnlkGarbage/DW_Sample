4357f9a1b81f35ea7f6d415b5a5d46f6108b97f7
dmnlk <seikima2demon@gmail.com>
2015-07-29 00:56:26 +0900

    write query

--- dws/src/main/java/com/dmnlk/doma/dao/TAccountDao.java
+++ dws/src/main/java/com/dmnlk/doma/dao/TAccountDao.java
@@ -7,6 +7,6 @@ import org.skife.jdbi.v2.sqlobject.SqlQuery;
  * @author dmnlk.
  */
 public interface TAccountDao {
-    @SqlQuery
+    @SqlQuery("select account_id from t_account where account_id = :account_id")
     Integer findById(@Bind("account_id") int id);
 }
