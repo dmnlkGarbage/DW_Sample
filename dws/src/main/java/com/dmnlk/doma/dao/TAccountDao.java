b0e7d8968982221a28ebb0e022b73ccf6d5fdb4d
dmnlk <seikima2demon@gmail.com>
2015-07-29 00:53:38 +0900

    change to interface

--- dws/src/main/java/com/dmnlk/doma/dao/TAccountDao.java
+++ dws/src/main/java/com/dmnlk/doma/dao/TAccountDao.java
@@ -1,7 +1,12 @@
 package com.dmnlk.doma.dao;
 
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+
 /**
  * @author dmnlk.
  */
-public class TAccountDao {
+public interface TAccountDao {
+    @SqlQuery
+    Integer findById(@Bind("account_id") int id);
 }
