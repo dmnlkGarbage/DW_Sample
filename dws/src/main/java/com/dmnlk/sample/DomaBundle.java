f4fe09815bf879d4a1f2cfd6969b32bb7b4e6b3e
dmnlk <seikima2demon@gmail.com>
2015-08-02 23:39:23 +0900

    doma bundle

--- dws/src/main/java/com/dmnlk/sample/DomaBundle.java
+++ dws/src/main/java/com/dmnlk/sample/DomaBundle.java
@@ -1,7 +1,41 @@
 package com.dmnlk.sample;
 
+import com.dmnlk.doma.AppConfig;
+import io.dropwizard.Configuration;
+import io.dropwizard.ConfiguredBundle;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.db.DatabaseConfiguration;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
 /**
  * @author dmnlk.
  */
-public class DomaBundle {
+public class DomaBundle<CONFIGURATION extends Configuration> implements ConfiguredBundle<CONFIGURATION>, DatabaseConfiguration<CONFIGURATION> {
+    protected static final String DEFAULT_DATASOURCE_NAME = "doma";
+    protected final String datasourceName;
+    protected AppConfig appConfig;
+
+    public DomaBundle() {
+        this(DEFAULT_DATASOURCE_NAME);
+    }
+
+    public DomaBundle(String datasourceName) {
+        this.datasourceName = datasourceName;
+    }
+
+    @Override
+    public void run(CONFIGURATION configuration, Environment environment) throws Exception {
+
+    }
+
+    @Override
+    public void initialize(Bootstrap<?> bootstrap) {
+
+    }
+
+    @Override
+    public DataSourceFactory getDataSourceFactory(CONFIGURATION configuration) {
+        return null;
+    }
 }
